9327a66fac64c53636819f2f44e7e0e991a1ae0b
supppppppbro <113025477+supppppppbro@users.noreply.github.com>
2023-12-06 15:34:28 +0300

    Create precode.go

---
+++ precode.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "strconv"
+    "strings"
+    "testing"
+)
+
+var cafeList = map[string][]string{
+    "moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
+}
+
+func mainHandle(w http.ResponseWriter, req *http.Request) {
+    countStr := req.URL.Query().Get("count")
+    if countStr == "" {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte("count missing"))
+        return
+    }
+
+    count, err := strconv.Atoi(countStr)
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte("wrong count value"))
+        return
+    }
+
+    city := req.URL.Query().Get("city")
+
+    cafe, ok := cafeList[city]
+    if !ok {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte("wrong city value"))
+        return
+    }
+
+    if count > len(cafe) {
+        count = len(cafe)
+    }
+
+    answer := strings.Join(cafe[:count], ",")
+
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(answer))
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+    totalCount := 4
+    req := ... // здесь нужно создать запрос к сервису
+
+    responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
+
+    // здесь нужно добавить необходимые проверки
+}
